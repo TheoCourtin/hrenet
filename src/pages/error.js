08c3cf7d62ed8cf730df56068e965475c50ecb7f
TheoCourtin <theo.joseph.courtin@gmail.com>
2023-11-30 14:19:08 +0100

    Structure des pages home et error

--- src/pages/error.js
+++ src/pages/error.js
@@ -1,7 +1,22 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import img404 from "../assets/404.png";
 
-const error = () => {
-  return <div>error</div>;
+const Error = () => {
+  return (
+    <main className="main">
+      <div className="error">
+        <div className="img-container">
+          <img src={img404} alt="a person parachuting" />
+        </div>
+
+        <h1 className="title-error">Error 404</h1>
+        <p className="error-text">La page que vous demandez n'existe pas.</p>
+        <button className="btn-primary">
+          <Link to="/">Retournez à la page d'accueil</Link>
+        </button>
+      </div>
+    </main>
+  );
 };
 
-export default error;
+export default Error;
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,29 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import home from "../assets/home.png";
 
-const home = () => {
-  return <div>home</div>;
+const Home = () => {
+  return (
+    <main className="hero">
+      <div className="container left">
+        <h1 className="title">Welcome to the WealthHealth HRnet.</h1>
+        <p className="content">
+          Our internal web application manages the files of our employees. Here
+          you can add a new employee. As well as looking at the list of
+          employees.
+        </p>
+        <button className="btn-primary">
+          <Link to={"/create-employee"}>Create an employee</Link>
+        </button>
+      </div>
+      <div className="container right">
+        <img
+          className="img"
+          src={home}
+          alt="Illustration of a Woman Sorting Files"
+        />
+      </div>
+    </main>
+  );
 };
 
-export default home;
+export default Home;

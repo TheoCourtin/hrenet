60edf415c51d4344f8cdd5848c61be6619f06650
TheoCourtin <theo.joseph.courtin@gmail.com>
2023-12-08 09:01:24 +0100

    Ajout du material UI + debut de list employee

--- src/pages/listEmployee.js
+++ src/pages/listEmployee.js
@@ -1,7 +1,15 @@
 import React from "react";
+import Table from "../components/table";
 
 const ListEmployee = () => {
-  return <div>ListEmployee</div>;
+  return (
+    <main className="employee-list">
+      <section>
+        <h1 className="h1">Employee List</h1>
+        <Table />
+      </section>
+    </main>
+  );
 };
 
 export default ListEmployee;
